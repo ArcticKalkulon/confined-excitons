1d52ccc9ebb08ef5bac86f17150e86e84631e4c0
ArcticKalkulon <dkalke@protonmail.com>
2022-10-16 19:23:29 +0200

    started implementing the root finding part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scipy.special import jv, yv
+from scipy.optimize import newton
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -66,21 +67,50 @@ def return_function(E, V0, r0):
     right = imag*AV/AW*(E+delta/2)/(V0-E-delta/2)*(np.imag(yv(1,-r0*AV)))/(jv(1,r0*AW))
     return left-right
 
-V0 = 30e-3 #eV
-r0 = 100 #Angström
+#V0 = 30e-3 #eV
+#r0 = 100 #Angström
 
-x = np.linspace(delta/2, V0+delta/2, 1000000, endpoint=True)
-x = x[1:-1]
-y = []
-for index, x_i in enumerate(x):
-    print(int(index/len(x)*100))
-    y.append(return_function(x_i, V0, r0))
-y = np.array(y)
-print(y)
+V0_list = np.linspace(0, 50e-3, 100, endpoint=True) # eV
+V0_list = V0_list[1:]
+r0_list = np.linspace(0, 5000, 100, endpoint=True) # Angström
+r0_list = r0_list[1:]
+EG = np.zeros((len(V0_list), len(r0_list)))
 
-plt.scatter((x-delta/2-V0)/V0,y)
-plt.ylabel("Root function")
-plt.xlabel("E/V0")
-plt.grid(True)
+
+for index_V0, V0 in enumerate(V0_list):
+    print(index_V0,"/",len(V0_list))
+    x = np.linspace(delta/2, V0+delta/2, 10000, endpoint=True)
+    x = x[1:-1]
+    newton_startpoint = np.linspace(np.min(x),np.max(x),20)
+    for index_r0, r0 in enumerate(r0_list):
+        min_root = np.max(x)
+        for x0 in newton_startpoint:
+            if x0 > min_root:
+                break
+            try:
+                root = newton(func=return_function, x0=x0, args=(V0,r0), maxiter=100)
+            except RuntimeError:
+                print(V0,r0)
+            if root < min_root:
+                min_root = root
+        EG[index_V0][index_r0] = min_root
+            
+                
+plt.imshow(EG)
 plt.show()
+            
+            
+        
+#y = []
+#for index, x_i in enumerate(x):
+#    print(int(index/len(x)*100),"/100")
+#    y.append(return_function(x_i, V0, r0))
+#y = np.array(y)
+#print(y)
+
+#plt.scatter((x-delta/2-V0)/V0,y)
+#plt.ylabel("Root function")
+#plt.xlabel("E/V0")
+#plt.grid(True)
+#plt.show()
 
